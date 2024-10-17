50007f738277502deddd2293c0d0791710bc62d1
Uma Maheswar <umamaheswar.maram@gmail.com>
2024-10-16 23:21:24 -0400

    More projects added and appearance changed
    1. Now projection has six projects with next and previous buttons
    2. Color theme of the website changed
    3. Now arrow shows wiggle animation
    4. In Experience section skills are rearranged

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+let currentPage  =1;
+const itemsPerPage = 3;
+
+function showPage(page) {
+    const allProjects = document.querySelectorAll('.project-content-container');
+    const totalProjects = allProjects.length;
+
+    allProjects.forEach((project, index) => {
+        project.classList.add('hidden');
+
+        if (index >= (page - 1) * itemsPerPage && index < page * itemsPerPage) {
+            project.classList.remove('hidden');
+        }
+    });
+    document.getElementById('prev').style.display = page === 1 ? 'none' : 'inline';
+    document.getElementById('next').style.display = page * itemsPerPage >= totalProjects ? 'none' : 'inline';
+}
+
+function scrollToProjects() {
+    const projectSection = document.getElementById('projects'); // Get the project section
+    projectSection.scrollIntoView({ behavior: 'smooth' }); // Scroll smoothly to the project section
+}
+
+function nextPage() {
+    currentPage++;
+    showPage(currentPage);
+    scrollToProjects();
+}
+
+function prevPage() {
+    currentPage--;
+    showPage(currentPage);
+    scrollToProjects();
+}
+
+showPage(currentPage);
